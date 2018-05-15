2b58002c503b0f41267e49deed13b7b0b1fe20c9
Tom Swindell <tom@zippie.org>
2018-05-15 17:06:11 +0100

    Initial commit
    Signed-off-by: Tom Swindell <tom@zippie.org>

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM node
+
+WORKDIR /app
+
+# Add sources
+ADD index.html /app/index.html
+ADD build /app/build
+ADD package.json /app/package.json
+ADD config /app/config
+ADD src /app/src
+ADD static /app/static
+ADD test /app/test
+ADD LICENSE /app/LICENCE
+ADD LICENSE.artwork /app/LICENCE.artwork
+ADD .babelrc /app/.babelrc
+ADD .editorconfig /app/.editorconfig
+ADD .eslintignore /app/.eslintignore
+ADD .eslintrc.js /app/.eslintrc.js
+ADD .postcssrc.js /app/.postcssrc.js
+
+
+# Install dependencies
+RUN npm install
+
+# Build
+RUN npm run build
---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <h1 style='margin: 1em; text-align: center; font-weight: 200'>QR Code Scanner</h1>
+
+    <qrcode-reader style='margin: 0 auto;' :video-constraints='{ facingMode: "environment" }' @decode='onDecode'></qrcode-reader>
+
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2016</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {
+    onDecode (data) {
+      if (data.startsWith('http://') || data.startsWith('https://')) {
+        window.location = data
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
